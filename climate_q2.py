5a98439820185ef0907ea397a7ba232f830fbf42
crashbox-hub <61722658+crashbox-hub@users.noreply.github.com>
2023-09-11 11:52:05 +1000

    Updated git_txt.txt
    climate_q2.py
    - loaded .csv data
    - extracted columns into seperate lists

--- climate_q2.py
+++ climate_q2.py
@@ -1,20 +1,23 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-years = []
-co2 = []
-temp = []
+# Load data from .csv file
+data_csv = pd.read_csv('climate.csv')
+
+# Extract into separate lists
+years = data_csv['Year']
+co2 = data_csv['CO2']
+temp = data_csv['Temperature']
 
 plt.subplot(2, 1, 1)
-plt.plot(years, co2, 'b--') 
-plt.title("Climate Data") 
-plt.ylabel("[CO2]") 
-plt.xlabel("Year (decade)") 
+plt.plot(years, co2, 'b--')
+plt.title("Climate Data")
+plt.ylabel("[CO2]")
+plt.xlabel("Year (decade)")
 
 plt.subplot(2, 1, 2)
-plt.plot(years, temp, 'r*-') 
-plt.ylabel("Temp (C)") 
-plt.xlabel("Year (decade)") 
-plt.show() 
-plt.savefig("co2_temp_2.png") 
-
+plt.plot(years, temp, 'r*-')
+plt.ylabel("Temp (C)")
+plt.xlabel("Year (decade)")
+plt.show()
+plt.savefig("co2_temp_2.png")
